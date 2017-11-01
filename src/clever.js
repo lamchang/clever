e3d2416634cbd5f5289893949ea4e37af194ea9e
lamchang <alanlamchang@gmail.com>
2017-10-31 23:30:56 -0600

    Ajustes dropdown functions

--- src/clever.js
+++ src/clever.js
@@ -5,11 +5,19 @@
  * @url
  */
 {
+
     var cleverCount = 0;
+    var clever;
 
     class Clever {
+
+
+
+
         constructor(element, options) {
-            var classs = this;
+
+            clever = this;
+
             this.DOM = {};
             this.DOM.element = element;
             this.options = {
@@ -42,7 +50,6 @@
 
         // Build
         _build () {
-
             this.DOM.element.init = true;
             this.DOM.element.classList.add ('clever--hide', 'clever-initialized');
             this.DOM.element.currentOption = this.InitialOption();
@@ -57,23 +64,23 @@
             cleverCount++;
 
             // Icon
-            this.DOM.element.select.icon = document.createElement ('i');
-            this.DOM.element.select.icon.classList.add ('clever__icon', 'clever-icon-dropdown');
+            this.DOM.element.icon = document.createElement ('i');
+            this.DOM.element.icon.classList.add ('clever__icon', 'clever-icon-dropdown');
 
             // Field
-            this.DOM.element.select.field = document.createElement ('button');
-            this.DOM.element.select.field.type = 'button';
-            this.DOM.element.select.field.role = 'button';
-            this.DOM.element.select.field.className = 'clever__field';
+            this.DOM.element.field = document.createElement ('button');
+            this.DOM.element.field.type = 'button';
+            this.DOM.element.field.role = 'button';
+            this.DOM.element.field.className = 'clever__field';
 
             // Current option
-            this.DOM.element.select.current = document.createElement ('p');
-            this.DOM.element.select.current.className = 'clever__current-option';
-            this.DOM.element.select.current.innerHTML = this.DOM.element.currentOption;
+            this.DOM.element.current = document.createElement ('p');
+            this.DOM.element.current.className = 'clever__current-option';
+            this.DOM.element.current.innerHTML = this.DOM.element.currentOption;
 
             // Dropdown
-            this.DOM.element.select.dropdown = document.createElement ('div');
-            this.DOM.element.select.dropdown.className = 'clever__dropdown';
+            this.DOM.element.dropdown = document.createElement ('div');
+            this.DOM.element.dropdown.className = 'clever__dropdown';
 
             // List
             this.DOM.element.select.list = document.createElement ('ul');
@@ -124,29 +131,22 @@
                 this.classList.add ('clever--active');
                 if(this.parent.options.linked) this.parent.DOM.element.allOptions[this.dataset.index].selected = true;
                 this.parent.DOM.element.currentOption = this.dataset.value;
-                this.parent.DOM.element.select.current.innerHTML = this.querySelector('.clever__option__value').innerHTML;
+                this.parent.DOM.element.current.innerHTML = this.querySelector('.clever__option__value').innerHTML;
                 this.parent.DOM.element.select.dataset.value = this.dataset.value;
             }
 
             // Appends
             this.DOM.element.parentNode.insertBefore (this.DOM.element.select, this.DOM.element);
             this.DOM.element.select.appendChild (this.DOM.element);
-            this.DOM.element.select.appendChild (this.DOM.element.select.field);
-            this.DOM.element.select.field.appendChild (this.DOM.element.select.current);
-            this.DOM.element.select.field.appendChild (this.DOM.element.select.icon);
-            this.DOM.element.select.appendChild (this.DOM.element.select.dropdown);
-            this.DOM.element.select.dropdown.appendChild (this.DOM.element.select.list);
+            this.DOM.element.select.appendChild (this.DOM.element.field);
+            this.DOM.element.field.appendChild (this.DOM.element.current);
+            this.DOM.element.field.appendChild (this.DOM.element.icon);
+            this.DOM.element.select.appendChild (this.DOM.element.dropdown);
+            this.DOM.element.dropdown.appendChild (this.DOM.element.select.list);
 
             // Events
-            document.addEventListener('click', this.HideDropdown);
-            this.DOM.element.select.addEventListener ('click', this.Dropdown);
-            var tempThis = this;
-            var tempEl = this.DOM.element
-            this.DOM.element.select.addEventListener ('click', function(){
-                tempThis.CheckValue (tempEl);
-            });
-
-            var classs = this;
+            document.addEventListener('click', this.closeDropdown);
+            this.DOM.element.select.addEventListener ('click', this.openDropdown);
 
         }
 
@@ -177,7 +177,7 @@
             }
         }
 
-        Dropdown () {
+        openDropdown () {
             var selects = document.querySelectorAll ('.clever');
 
             for (var i = 0; i < selects.length; i++) {
@@ -189,11 +189,11 @@
 
             }
 
-            this.classList.toggle ('clever--focus');
-            this.dropdown.classList.toggle ('clever--active');
+            clever.DOM.element.select.classList.toggle ('clever--focus');
+            clever.DOM.element.dropdown.classList.toggle ('clever--active');
         }
 
-        HideDropdown () {
+        closeDropdown () {
             if (event.target.closest('.clever') == null) {
 
                 var selects = document.querySelectorAll ('.clever');
@@ -220,34 +220,33 @@
 
         // Change de dropdown position
         AutoPosition () {
-            var clever = this;
 
             // Catch event "scroll"
             window.addEventListener('scroll', function() {
 
                 // If window scroll is bigger or equal to select position...
-                if( window.pageYOffset > clever.DOM.element.select.dropdown.getBoundingClientRect().top ) {
+                if( window.pageYOffset > clever.DOM.element.dropdown.getBoundingClientRect().top ) {
 
                     // ...change the dropdown position
-                    clever.DOM.element.select.dropdown.style.top = '100%';
-                    clever.DOM.element.select.dropdown.style.bottom = 'initial';
+                    clever.DOM.element.dropdown.style.top = '100%';
+                    clever.DOM.element.dropdown.style.bottom = 'initial';
 
                 }
-                else if(window.pageYOffset < clever.DOM.element.select.dropdown.getBoundingClientRect().top + clever.DropdownSize()) {
+                else if(window.pageYOffset < clever.DOM.element.dropdown.getBoundingClientRect().top + clever.DropdownSize()) {
 
                     // ...change the dropdown position
-                    clever.DOM.element.select.dropdown.style.top = 'initial';
-                    clever.DOM.element.select.dropdown.style.bottom = '100%';
-                    
+                    clever.DOM.element.dropdown.style.top = 'initial';
+                    clever.DOM.element.dropdown.style.bottom = '100%';
+
                 }
             });
-            
+
         }
 
         DropdownSize () {
             var optionsCount = this.DOM.element.allOptions.length;
             var optionHeight = this.DOM.element.select.list.querySelector('.clever__option:first-of-type').offsetHeight;
-            
+
             return optionsCount * optionHeight;
         }
 
